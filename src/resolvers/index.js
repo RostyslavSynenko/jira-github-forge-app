7b5a6f3fea510ab5a1e4a2cf6c58b7eafc93482a
Rostyslav Synenko <rsynenko@atlassian.com>
2025-09-15 20:16:14 +0300

    [Token] Implemented getting, setting, deleting token functionality. Updated Eslint.

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,29 @@
 import Resolver from '@forge/resolver';
+import { getStorageData, setStorageData, deleteStorageData } from '../services';
 
 const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
   console.log(req);
   return 'Hello, world!';
+});
+
+resolver.define('getStorageData', async (req) => {
+  const { key } = req.payload;
+
+  return getStorageData(key);
+});
+
+resolver.define('setStorageData', async (req) => {
+  const { key, value } = req.payload;
+
+  return setStorageData(key, value);
+});
+
+resolver.define('deleteStorageData', async (req) => {
+  const { key } = req.payload;
+
+  return deleteStorageData(key);
 });
 
 export const handler = resolver.getDefinitions();
---
+++ ui/components/Token.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  ErrorMessage,
+  Box,
+  Label,
+  Textfield,
+  LoadingButton,
+  RequiredAsterisk,
+} from '@forge/react';
+import { GITHUB_API_TOKEN_STORAGE_KEY } from '../constants';
+import { setStorageData, deleteStorageData } from '../services';
+
+export const Token = ({ token, handleSetToken }) => {
+  const [validationError, setValidationError] = useState(null);
+  const [isMutatingGithubTokenLoading, setIsMutatingGithubTokenLoading] =
+    useState(false);
+  const [githubTokenInput, setGithubTokenInput] = useState('');
+
+  const handleChangeTokenInput = (event) => {
+    const value = event.target.value;
+
+    setGithubTokenInput(value);
+  };
+
+  const handleSetStorageData = async () => {
+    setIsMutatingGithubTokenLoading(true);
+
+    try {
+      if (!githubTokenInput) {
+        setValidationError('Token cannot be empty!');
+      } else {
+        setValidationError(null);
+
+        await setStorageData(GITHUB_API_TOKEN_STORAGE_KEY, githubTokenInput);
+
+        setGithubTokenInput('');
+        handleSetToken(githubTokenInput);
+      }
+    } catch (error) {
+      console.error({ error }, 'Failed to save token');
+    } finally {
+      setIsMutatingGithubTokenLoading(false);
+    }
+  };
+
+  const handleDeleteStorageData = async () => {
+    setIsMutatingGithubTokenLoading(true);
+
+    try {
+      await deleteStorageData(GITHUB_API_TOKEN_STORAGE_KEY);
+
+      handleSetToken(null);
+    } catch (error) {
+      console.error({ error }, 'Failed to save token');
+    } finally {
+      setIsMutatingGithubTokenLoading(false);
+    }
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Text>No token...</Text>
+        <Box xcss={{ width: '500px' }} paddingBlockStart="space.500">
+          <Label labelFor="token-textfield">
+            Add GitHub token <RequiredAsterisk />
+          </Label>
+          <Textfield
+            name="token-textfield"
+            onChange={handleChangeTokenInput}
+            value={githubTokenInput}
+          />
+          {validationError && <ErrorMessage>{validationError}</ErrorMessage>}
+
+          <LoadingButton
+            onClick={handleSetStorageData}
+            isLoading={isMutatingGithubTokenLoading}
+          >
+            Add
+          </LoadingButton>
+        </Box>
+      </>
+    );
+  }
+
+  return (
+    <Box xcss={{ width: '500px' }} paddingBlockStart="space.500">
+      <Text>Delete GitHub token </Text>
+      <LoadingButton
+        onClick={handleDeleteStorageData}
+        isLoading={isMutatingGithubTokenLoading}
+      >
+        Delete
+      </LoadingButton>
+    </Box>
+  );
+};
